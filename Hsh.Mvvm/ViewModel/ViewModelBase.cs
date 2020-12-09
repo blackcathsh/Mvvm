626fe0b621d552144cb1902a4f9f5c6fa7a360ab
huangshenghua <streetlighthsh@outlook.com>
2020-12-09 21:35:48 +0800

    Dev ViewModelBase

---
+++ Hsh.Mvvm/ViewModel/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Hsh.Mvvm.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool SetProperty<T>(ref T item, T value, string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(item, value))
+            {
+                return false;
+            }
+
+            item = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
